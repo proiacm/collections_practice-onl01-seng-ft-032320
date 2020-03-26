1ba5d2555c6c220abd87f677a4e57dc2f50afbcc
CiaraMaria Proia <proiacm@gmail.com>
2020-03-26 20:00:39 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -11,5 +11,5 @@ def sort_array_char_count(num)
 end  
 
 def swap_elements(dif)
-  swap_elements.sort {|x, y| x(1), y(2) = x(2), y(1)}
-end  
+  swap_elements.sort {|x, y| (1), (2) = (2), (1)}
+end 
